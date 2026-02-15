38307122bd49d688963cd5b8f3025ba627090345
Rameel Haroon <RameelHaroonOff@gmail.com>
2026-02-15 12:57:04 +0500

    Fixed failing test

--- src/test/java/com/rameel/To_Do_List_API/ToDoListApiApplicationTests.java
+++ src/test/java/com/rameel/To_Do_List_API/ToDoListApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.rameel.To_Do_List_API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToDoListApiApplicationTests {
 
 	@Test
